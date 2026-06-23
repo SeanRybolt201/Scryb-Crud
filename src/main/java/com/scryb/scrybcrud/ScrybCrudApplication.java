308d26f60ee3dcb0fd63c55835e9809115891b9a
Sean Rybolt <seanrybolt18@gmail.com>
2026-06-22 23:56:01 -0500

    Initial commit

---
+++ src/main/java/com/scryb/scrybcrud/ScrybCrudApplication.java
@@ -0,0 +1,12 @@
+package com.scryb.scrybcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrybCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrybCrudApplication.class, args);
+    }
+}
---
+++ src/test/java/com/scryb/scrybcrud/ScrybCrudApplicationTests.java
@@ -0,0 +1,12 @@
+package com.scryb.scrybcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrybCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
